869a812b31c1f246e9fb6941f572b6057920b9f8
Alex Kaganovich <alex.kaganovich@AlexMacBookPro.local>
2020-04-09 21:02:03 +0300

    implement naive health check using actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/health")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("Ok")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(health_check)})
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
